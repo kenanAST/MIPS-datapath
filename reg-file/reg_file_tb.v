603e4415c9ab4d0e04c3dfbeb71c058fa2deae52
kenanAST <kenanjoshua3454@gmail.com>
2022-12-06 12:42:10 +0800

    feat: added clk signal

--- reg-file/reg_file_tb.v
+++ reg-file/reg_file_tb.v
@@ -1,28 +1,33 @@
-`timescale 1ns / 1ps
 `include "reg_file.v"
 
 
 module reg_file_tb();
-
+  reg clk, rst;
   reg [4:0] R1, R2;
   reg [4:0] W1;
   reg [31:0] D1;
   wire [31:0] Out1, Out2;
 
 
-  reg_file register_file(R1, R2, W1, D1, Out1, Out2);
+  reg_file register_file(clk, rst, R1, R2, W1, D1, Out1, Out2);
+
+  always #1 clk = ~clk;
 
   initial begin 
-
+    
     $dumpfile("reg_file_tb.vcd");
     $dumpvars(0, reg_file_tb);
+
+    clk <= 0;
+    rst <= 1;
+    #1
+    rst<=0;
+    #1
     R1 = 2; R2 = 1; W1 = 2; D1 = 25;
     $display("%d %d %d %d %d", R1, R2, W1, Out1, Out2);
     #1
-    R1 = 5; R2 = 5; W1 = 5; D1 = 3;
     $display("%d %d %d %d %d", R1, R2, W1, Out1, Out2);
     #1
-    R1 = 5; R2 = 5; W1 = 5; D1 = 3;
     $display("%d %d %d %d %d", R1, R2, W1, Out1, Out2);
     #15 $finish;
   end
